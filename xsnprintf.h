92e03a68e7647dba6891de95438f81ec0ba764cc
Michael Clark <michaeljclark@mac.com>
2025-02-19 16:14:01 +1300

    move code inline and instantiate xsnprintf in module

--- xsnprintf.h
+++ xsnprintf.h
@@ -25,6 +25,7 @@
 
 #pragma once
 
+#include <stddef.h>
 #include <stdarg.h>
 #include <string.h>
 
@@ -253,13 +254,117 @@ static inline size_t utoa_u64(ullong n, char *s)
     return dig;
 }
 
-int xvsnprintf(char * restrict out, size_t n, const char* fmt, va_list vl);
+static const char *hexdigits;
 
-static inline int xsnprintf(char *out, size_t n, const char *fmt, ...)
+static inline size_t xvappend_char(char *restrict out, size_t o, size_t n,
+	char c)
 {
-    va_list vl;
-    va_start(vl, fmt);
-    int res = xvsnprintf(out, n, fmt, vl);
-    va_end(vl);
-    return res;
+    if (out && o < n) {
+        out[o] = c;
+    }
+    return o + 1;
 }
+
+static inline size_t xvappend_hex_u32(char *restrict out, size_t o, size_t n,
+    uint val)
+{
+    size_t dig = (32 - __builtin_clz(val) + 3) / 4;
+    for (size_t i = dig - 1; i < dig; i--) {
+        o = xvappend_char(out, o, n, hexdigits[(val >> (i * 4)) & 0xf]);
+    }
+    return o;
+}
+
+static inline size_t xvappend_hex_u64(char *restrict out, size_t o, size_t n,
+    ullong val)
+{
+    size_t dig = (64 - __builtin_clzll(val) + 3) / 4;
+    for (size_t i = dig - 1; i < dig; i--) {
+        o = xvappend_char(out, o, n, hexdigits[(val >> (i * 4)) & 0xf]);
+    }
+    return o;
+}
+
+static inline int xvsnprintf(char *restrict out, size_t n, const char* fmt,
+    va_list vl)
+{
+    int w = -1, s = 0, c;
+    size_t o = 0, l;
+    const char *v;
+    char t[21];
+    for ( ; *fmt; fmt++) {
+        if (w >= 0) {
+            switch (*fmt) {
+            case 'l':
+                w = w < 2 ? w + 1 : w;
+                break;
+            case 'd':
+                s = 1;
+                /* fallthrough */
+            case 'u':
+                if (w == 0 || sizeof(long) == 4) {
+                    int val = va_arg(vl, int);
+                    if (s && val < 0) {
+                        val = -val;
+                        o = xvappend_char(out, o, n, '-');
+                    }
+                    l = utoa_u32(val, t);
+                }
+                else if (w == 2 || sizeof(long) == 8) {
+                    llong val = va_arg(vl, llong);
+                    if (s && val < 0) {
+                        val = -val;
+                        o = xvappend_char(out, o, n, '-');
+                    }
+                    l = utoa_u64(val, t);
+                }
+                l = n - o < l ? n - o : l;
+                memcpy(out + o, t, l);
+                o += l;
+                w = -1;
+                s = 0;
+                break;
+            case 'p':
+                o = xvappend_char(out, o, n, '0');
+                o = xvappend_char(out, o, n, 'x');
+                w = 2;
+                /* fallthrough */
+            case 'x':
+                if (w == 0 || sizeof(long) == 4) {
+                    int val = va_arg(vl, int);
+                    o = xvappend_hex_u32(out, o, n, val);
+                }
+                else if (w == 2 || sizeof(long) == 8) {
+                    llong val = va_arg(vl, llong);
+                    o = xvappend_hex_u64(out, o, n, val);
+                }
+                w = -1;
+                break;
+            case 's':
+                v = va_arg(vl, const char *);
+                l = strlen(v);
+                memcpy(out + o, v, n - o < l ? n - o : l);
+                o += l;
+                w = -1;
+                break;
+            case 'c':
+                c = va_arg(vl, int);
+                o = xvappend_char(out, o, n, (char)c);
+                w = -1;
+                break;
+            }
+        }
+        else if (*fmt == '%') {
+            w = 0;
+        }
+        else {
+            o = xvappend_char(out, o, n, *fmt);
+        }
+    }
+    if (out) {
+        out[o < n ? o : n ? n - 1 : 0] = 0;
+    }
+    return o;
+}
+
+int xsnprintf(char *out, size_t n, const char *fmt, ...);
